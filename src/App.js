9877e0cf4a6685071b92a1b449ea10e72a691f8d
15202-sidorov <iliasidorov90@gmail.com>
2018-10-18 16:07:49 +0700

    Drawer added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import LC from "literallycanvas";
+
 import './App.css';
+
+import "../node_modules/literallycanvas/lib/css/literallycanvas.css"
 
 const App = ({type, word}) => {
   return(
     <div className="App">
       <div className="InputForm">
-        <div className="LeftContainer"> 
+        <div className="LeftContainer">
           <h1>{ word }</h1>
         </div>
-        <div className="CentralContainer"> 
+        <div className="CentralContainer">
+            <LC.LiterallyCanvasReactComponent imageURLPrefix="./img" />
         </div>
-        <div className="RightContainer"> 
+        <div className="RightContainer">
         </div>
       </div>
     </div>
